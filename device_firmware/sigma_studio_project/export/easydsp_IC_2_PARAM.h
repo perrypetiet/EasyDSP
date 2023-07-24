0fc8ad94aaad254dc9277731a3237bdf0a578f7d
Perry <perrypetiet@gmail.com>
2023-07-24 20:10:33 +0200

    Tested parameter header file generation
    Tested the generation of the header file from a sigmastudio project using an existing script. Working! can upload a sigmastudio project using MCUdudes library.

---
+++ device_firmware/sigma_studio_project/export/easydsp_IC_2_PARAM.h
@@ -0,0 +1,23 @@
+/*
+ * File:           C:\Users\perry\Documents\GitHub\EasyDSP\device_firmware\sigma_studio_project\export\easydsp_IC_2_PARAM.h
+ *
+ * Created:        Monday, July 24, 2023 7:13:49 PM
+ * Description:    easydsp:IC 2 parameter RAM definitions.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * This software may only be used to program products purchased from
+ * Analog Devices for incorporation by you into audio products that
+ * are intended for resale to audio product end users. This software
+ * may not be distributed whole or in any part to third parties.
+ *
+ * Copyright ©2023 Analog Devices, Inc. All rights reserved.
+ */
+#ifndef __EASYDSP_IC_2_PARAM_H__
+#define __EASYDSP_IC_2_PARAM_H__
+
+
+#endif
